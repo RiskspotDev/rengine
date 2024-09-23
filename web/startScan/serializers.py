dc2c2d57ab00f31de10badf06c3b0100bac9ba93
Joshua Gato <joshuagato37@gmail.com>
2024-09-23 01:34:32 +0000

    Added new serializers start scan app serializers file

--- web/startScan/serializers.py
+++ web/startScan/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import ScanHistory
+from .models import ScanHistory, ScanActivity, CountryISO
 
 
 class ScanHistorySerializer(serializers.ModelSerializer):
@@ -7,4 +7,28 @@ class ScanHistorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ScanHistory
+        fields = '__all__'
+
+
+class MostRecentScanSerializer(serializers.ModelSerializer):
+    """ Serializer for the MostRecentScan model """
+
+    class Meta:
+        model = ScanHistory
+        exclude = ['start_scan_date', 'domain', 'scan_type', 'emails', 'employees', 'buckets', 'dorks']
+
+
+class ScanActivitySerializer(serializers.ModelSerializer):
+    """ Serializer for the ScanActivity model """
+
+    class Meta:
+        model = ScanActivity
+        exclude = ('title', 'name', 'time', 'status')
+
+
+class CountryISOSerializer(serializers.ModelSerializer):
+    """ Serializer for the CountryISO model """
+
+    class Meta:
+        model = CountryISO
         fields = '__all__'
