c5b3943b18fd9c194b668f302962b29bc97042f5
Joshua Gato <joshuagato37@gmail.com>
2024-09-25 14:30:55 +0000

    Added more serializers for fetching json data

--- web/startScan/serializers.py
+++ web/startScan/serializers.py
@@ -1,5 +1,18 @@
 from rest_framework import serializers
-from .models import ScanHistory, ScanActivity, CountryISO
+from .models import (ScanHistory,
+                     ScanActivity,
+                     CountryISO,
+                     Subdomain,
+                     IpAddress,
+                     Technology,
+                     DirectoryScan,
+                     Waf,
+                     Port,
+                     SubScan,)
+from targetApp.models import Domain
+
+from api.serializers import PortSerializer, SubdomainSerializer
+from scanEngine.models import EngineType
 
 
 class ScanHistorySerializer(serializers.ModelSerializer):
@@ -23,7 +36,7 @@ class ScanActivitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ScanActivity
-        exclude = ('title', 'name', 'time', 'status')
+        exclude = ('title', 'name', 'time', 'status',)
 
 
 class CountryISOSerializer(serializers.ModelSerializer):
@@ -31,4 +44,58 @@ class CountryISOSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CountryISO
+        fields = '__all__'
+
+class TechnologySerializer(serializers.ModelSerializer):
+    """ Serializer for the Technology model """
+
+    class Meta:
+        model = Technology
+        fields = '__all__'
+
+
+class SubScanSerializer(serializers.ModelSerializer):
+    """ Serializer for the Subscan model """
+
+    scan_history = ScanHistorySerializer(many=True, read_only=True)
+    subdomain = SubdomainSerializer()
+    engine = EngineType()
+    subdomain_subscan_ids = SubdomainSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = SubScan
+        fields = '__all__'
+
+
+class IpAddressSerializer(serializers.ModelSerializer):
+    """ Serializer for the IpAddress model """
+
+    ports = PortSerializer(many=True, read_only=True)
+    geo_iso = CountryISOSerializer(required=False)
+    ip_subscan_ids = SubScanSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = IpAddress
+        fields = '__all__'
+
+
+class DirectoryScanSerializer(serializers.ModelSerializer):
+    """ Serializer for the DirectoryScan model """
+
+    class Meta:
+        model = DirectoryScan
+        fields = '__all__'
+
+
+class WafSerializer(serializers.ModelSerializer):
+    """ Serializer for the Waf model """
+    class Meta:
+        model = Waf
+        fields = '__all__'
+
+
+class DomainSerializer(serializers.ModelSerializer):
+    """ Serializer for the DomainModel """
+    class Meta:
+        model = Domain
         fields = '__all__'
